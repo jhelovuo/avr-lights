d725ec46370b7bdc5cecdceb128cba104dcc51a6
Juhana Helovuo <juhe@iki.fi>
2024-10-19 18:26:13 +0300

    PWM LED brightness test program

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,13 @@ type CoreClock = atmega_hal::clock::MHz16;
 type Delay = atmega_hal::delay::Delay<crate::CoreClock>;
 
 // Below are examples of a delay helper functions
+#[allow(dead_code)]
 fn delay_ms(ms: u16) {
     Delay::new().delay_ms(u32::from(ms))
+}
+
+fn delay_us(us: u32) {
+    Delay::new().delay_us(us)
 }
 
 #[avr_device::entry]
@@ -20,11 +25,20 @@ fn main() -> ! {
     // Set Data Direction Register C to all pins output.
     dp.PORTC.ddrc.write(|w| unsafe {w.bits(0xFF)});
     
-    let mut my_byte = 0x01_u8;
+    let led_pwm_values :[u8;8] = [0 , 2, 4, 8, 16, 32, 64, 128];
 
     loop {
-        dp.PORTC.portc.write(|w| unsafe {w.bits(my_byte)});
-        my_byte = my_byte.rotate_left(1);
-        delay_ms(400);
+        //let mut pins_out: u8 = 0;
+        for i in u8::MIN ..= u8::MAX {
+            let pins_out = led_pwm_values.iter().fold(0, |mut out, limit| {
+                out >>= 1; 
+                if i < *limit {
+                    out |= 0b1000_0000;
+                }
+                out
+            });
+            dp.PORTC.portc.write(|w| unsafe {w.bits(pins_out)});
+            delay_us(10);
+        }
     }
 }
