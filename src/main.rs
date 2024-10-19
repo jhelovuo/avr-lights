c709539237f0a0016f40730995a9819fe3f99c1c
Juhana Helovuo <juhe@iki.fi>
2024-10-19 18:45:22 +0300

    Knight Rider effect

--- src/main.rs
+++ src/main.rs
@@ -25,20 +25,27 @@ fn main() -> ! {
     // Set Data Direction Register C to all pins output.
     dp.PORTC.ddrc.write(|w| unsafe {w.bits(0xFF)});
     
-    let led_pwm_values :[u8;8] = [0 , 2, 4, 8, 16, 32, 64, 128];
+    let mut led_pwm_values :[u8;8] = [0;8];
 
     loop {
-        //let mut pins_out: u8 = 0;
-        for i in u8::MIN ..= u8::MAX {
-            let pins_out = led_pwm_values.iter().fold(0, |mut out, limit| {
-                out >>= 1; 
-                if i < *limit {
-                    out |= 0b1000_0000;
+        for p in -7..8_i8 {
+            for _ in 0 .. 40 {
+                for i in u8::MIN ..= u8::MAX {
+                    let pins_out = led_pwm_values.iter().fold(0, |mut out, limit| {
+                        out >>= 1; 
+                        if i < *limit {
+                            out |= 0b1000_0000;
+                        }
+                        out
+                    });
+                    dp.PORTC.portc.write(|w| unsafe {w.bits(pins_out)});
+                    delay_us(2);
                 }
-                out
-            });
-            dp.PORTC.portc.write(|w| unsafe {w.bits(pins_out)});
-            delay_us(10);
+            }
+            // update pattern
+            led_pwm_values.iter_mut()
+                .for_each(|v| *v >>= 1 );
+            led_pwm_values[p.abs() as usize] = u8::MAX;
         }
     }
 }
